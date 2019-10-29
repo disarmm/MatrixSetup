17881da31752469ab5cf2d2e2ba90112f6201185
disarmm <rleonard85@gmail.com>
2019-10-29 07:51:37 +0000

    updated

--- MatrixInstaller.sh
+++ MatrixInstaller.sh
@@ -14,8 +14,10 @@ function banner(){
 clear
 banner
 lb
-echo "This installer will help you install your Matrix Node to any hard drive partition that has at least 300 GB of free space." | fold -s
+echo "This installer will help you install your Matrix Node to any hard drive partition that has at least 150 GB of free space." | fold -s
 lb
+echo "PLEASE READ EVERY PROMPT CAREFULLY TO AVOID DATA LOSS"
+sleep 1
 pause 'Press [Enter] key to continue...'
 
 # Define install functions
@@ -25,15 +27,38 @@ existingInstall=( $(find / -name gman ! -type d) )
 existingPath="Please confirm your current masternode install path:"
 lb
 PS3="$existingPath "
-select matrixPath in "${existingInstall[@]}" "Abort Install" ; do
+select gmanPath in "${existingInstall[@]}" "Abort Install" ; do
     if (( REPLY == 1 + ${#existingInstall[@]} )) ; then
 	exit
     elif (( REPLY > 0 && REPLY <= ${#existingInstall[@]} )) ; then
 	lb
 	echo "You have selected to update..."
-	echo $matrixPath
+	echo $gmanPath
 	lb
 	pause 'If this is correct, press [Enter] to confirm, or Ctrl+C to abort installation'
+	lb
+	echo "WARNING: This will erase your existing chaindata and replace it with the 1405031 snapshot"
+	sleep 1
+	pause '	   Press [Enter] again to confirm or Ctrl+C to abort'
+	rm $gmanPath/gman $gmanPath/MANGenesis.json $gmanPath/firstRun
+	mv $gmanPath/chaindata/keystore $gmanPath/keystore
+	rm -rf $gmanPath/chaindata $gmanPath/snapdir
+	wget www2.matrixainetwork.eu/snapshots/1405031.tar.gz -O $gmanPath/1405031.tar.gz && tar -zxvf $gmanPath/1405031.tar.gz -C $gmanPath/
+        wget https://github.com/MatrixAINetwork/GMAN_CLIENT/raw/master/MAINNET/1022/linux/gman -O $gmanPath/gman && chmod a+x $gmanPath/gman
+        wget https://raw.githubusercontent.com/MatrixAINetwork/GMAN_CLIENT/master/MAINNET/1022/MANGenesis.json -O $gmanPath/MANGenesis.json
+	mv $gmanPath/keystore $gmanPath/chaindata/keystore
+	clear
+	banner
+	lb
+	echo "Please enter your wallet B address to create startup script"
+	read manWallet
+        echo -e "if [ ! -f "$gmanPath/firstRun" ]; then\n      touch $gmanPath/firstRun && $gmanPath/gman --datadir $gmanPath/chaindata --networkid 1 --debug --verbosity 1 --port 50505 --manAddress $manWallet --entrust $gmanPath/entrust.json --gcmode archive --outputinfo 1 --syncmode full --loadsnapfile "TrieData1405031"\nelse\n    $gmanPath/gman --datadir $gmanPath/chaindata --networkid 1 --debug --verbosity 1 --port 50505 --manAddress $manWallet --entrust $gmanPath/entrust.json --gcmode archive --outputinfo 1 --syncmode full\nfi" > $gmanPath/gmanRunScript.sh
+        echo -e "alias gmanMatrix=/$gmanPath/gmanRunScript.sh" >> ~/.bashrc
+        source ~/.bashrc
+        clear
+        curl https://raw.githubusercontent.com/disarmm/MatrixSetup/master/completedBanner
+        lb
+        echo "Type gmanMatrix to start your new node"
 	break
     else
 	echo "Invalid option. Please try again."
@@ -43,7 +68,7 @@ done
 	
 function newNodeInstall()
 {
-newInstall=( $(df -h | grep /dev/ | grep -v "100%" | grep -v "tmpfs" | awk '$4 >= 100 {print}' | awk '$4 !~/M/ {print}' | awk 'length($4) >= 4 {print}' | awk '{print $6}') )
+newInstall=( $(df -h | grep /dev/ | grep -v "100%" | grep -v "tmpfs" | awk '$4 >= 150 {print}' | awk '$4 !~/M/ {print}' | awk 'length($4) >= 4 {print}' | awk '{print $6}') )
 newInstallPath="Select a path to install your new Matrix Node:"
 lb
 PS3="$newInstallPath "
@@ -73,7 +98,7 @@ select matrixPath in "${newInstall[@]}" "Abort Install" ; do
 	echo -e '[\n{\n"Address":"'$manWallet'",\n"Password":"'$manPasswd'"\n}\n]' > $matrixPath/matrix/signAccount.json
 	lb
 	echo "Downloading and installing files..."
-	sleep 5
+	sleep 2
 	wget www2.matrixainetwork.eu/snapshots/1405031.tar.gz -O $matrixPath/matrix/1405031.tar.gz && tar -zxvf $matrixPath/matrix/1405031.tar.gz -C $matrixPath/matrix/
         wget https://github.com/MatrixAINetwork/GMAN_CLIENT/raw/master/MAINNET/1022/linux/gman -O $matrixPath/matrix/gman && chmod a+x /$matrixPath/matrix/gman
         wget https://raw.githubusercontent.com/MatrixAINetwork/GMAN_CLIENT/master/MAINNET/1022/MANGenesis.json -O $matrixPath/matrix/MANGenesis.json
@@ -84,14 +109,14 @@ select matrixPath in "${newInstall[@]}" "Abort Install" ; do
 	echo "Creating entrust.json file..."
 	lb
 	echo "Note: Please choose a different password than your Wallet B password"
-	sleep 5
+	sleep 1
 	lb
 	pause '		Press [Return] to continue]'
 	$matrixPath/matrix/gman --datadir $matrixPath/matrix/chaindata aes --aesin $matrixPath/matrix/signAccount.json --aesout $matrixPath/matrix/entrust.json
 	echo "Open your downloaded UTC wallet file with wordpad or notepad++ and copy/paste the contents below"
 	read matrixKeystore
 	echo -e "$matrixKeystore" > $matrixPath/matrix/chaindata/keystore/wallet.file
-	echo -e "if [ ! -f "$matrixPath/matrix/firstRun" ]; then\n      touch $matrixPath/matrix/firstRun && $matrixPath/matrix/gman --datadir $matrixPath/matrix/chaindata --networkid 1 --debug --verbosity 1 --port 50505 --manAddress $manWallet --entrust /matrix/entrust.json --gcmode archive --outputinfo 1 --syncmode full --loadsnapfile "TrieData1405031"\nelse\n    $matrixPath/matrix/gman --datadir $matrixPath/matrix/chaindata --networkid 1 --debug --verbosity 1 --port 50505 --manAddress $manWallet --entrust $matrixPath/matrix/entrust.json --gcmode archive --outputinfo 1 --syncmode full\nfi" > $matrixPath/matrix/gmanRunScript.sh
+	echo -e "if [ ! -f "$matrixPath/matrix/firstRun" ]; then\n      touch $matrixPath/matrix/firstRun && $matrixPath/matrix/gman --datadir $matrixPath/matrix/chaindata --networkid 1 --debug --verbosity 1 --port 50505 --manAddress $manWallet --entrust /$matrixPath/matrix/entrust.json --gcmode archive --outputinfo 1 --syncmode full --loadsnapfile "TrieData1405031"\nelse\n    $matrixPath/matrix/gman --datadir $matrixPath/matrix/chaindata --networkid 1 --debug --verbosity 1 --port 50505 --manAddress $manWallet --entrust $matrixPath/matrix/entrust.json --gcmode archive --outputinfo 1 --syncmode full\nfi" > $matrixPath/matrix/gmanRunScript.sh
 	echo -e "alias gmanMatrix=/$matrixPath/gmanRunScript.sh" >> ~/.bashrc
 	source ~/.bashrc
 	clear
@@ -105,7 +130,7 @@ select matrixPath in "${newInstall[@]}" "Abort Install" ; do
 done
 }
 
-installType="Do you already have a Matrix node on this system?"
+installType="Do you already have a Matrix node installed on this system?"
 lb
 PS3="$installType "
 select yn in "Yes" "No"; do
